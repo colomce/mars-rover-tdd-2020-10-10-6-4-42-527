2fc15a8df67c4c90c341ce36ebb34a986843f8af
Cedric Don Coloma <colomce@oocl.com>
2020-10-15 18:22:11 +0800

    [MarsRover][Cedric Coloma] test: x 0, y 0, heading E when x: 0, y: 0, heading: N, commands: R

--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -124,4 +124,19 @@ class DemoTest {
         assertEquals(0, rover.getLocationY());
         assertEquals("S", rover.getHeading());
     }
+
+    @Test
+    void should_x_0_y_0_heading_E_when_runActions_given_x_0_y_0_heading_N_commands_R() {
+        //given
+        String commands = "R";
+        MarsRover rover = new MarsRover(0, 0, "N");
+
+        //when
+        rover.runActions(commands);
+
+        //then
+        assertEquals(0, rover.getLocationX());
+        assertEquals(0, rover.getLocationY());
+        assertEquals("E", rover.getHeading());
+    }
 }
