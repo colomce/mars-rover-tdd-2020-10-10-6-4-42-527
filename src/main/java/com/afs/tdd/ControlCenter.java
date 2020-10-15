e638aa8b89e91fb98dde243d5b31d7e35f66c01b
Cedric Don Coloma <colomce@oocl.com>
2020-10-16 00:10:50 +0800

    [MarsRover][Cedric Coloma] feature: Added functionality for invoker to rollback command.

--- src/main/java/com/afs/tdd/ControlCenter.java
+++ src/main/java/com/afs/tdd/ControlCenter.java
@@ -2,21 +2,28 @@ package com.afs.tdd;
 
 import com.afs.tdd.commands.ICommand;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class ControlCenter {
-    private List<ICommand> commands;
+    private List<ICommand> commands = new ArrayList<>();
 
     public ControlCenter(ICommand... commands) {
-        this.commands = Arrays.asList(commands);
+        this.commands.addAll(Arrays.asList(commands));
     }
 
     public List<ICommand> send() {
         return commands;
     }
 
-    public void rollback() {
-        
+    public List<ICommand> rollback() {
+        Collections.reverse(commands);
+        return commands;
+    }
+
+    public void removeCommand(List<ICommand> invalidCommands) {
+        commands.removeIf(invalidCommands::contains);
     }
 }
