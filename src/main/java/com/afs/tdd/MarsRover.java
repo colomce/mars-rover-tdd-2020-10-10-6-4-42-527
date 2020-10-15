55751b2bf53bc58bc544806e07ec64e72be7d82d
Cedric Don Coloma <colomce@oocl.com>
2020-10-15 18:31:25 +0800

    [MarsRover][Cedric Coloma] feauter: Added feature to set x 0, y 0, heading S when x: 0, y: 0, heading: E, commands: R

--- src/main/java/com/afs/tdd/MarsRover.java
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -57,6 +57,8 @@ public class MarsRover {
             heading = "E";
         } else if ("S".equals(heading)) {
             heading = "W";
+        } else if ("E".equals(heading)) {
+            heading = "S";
         }
     }
 
