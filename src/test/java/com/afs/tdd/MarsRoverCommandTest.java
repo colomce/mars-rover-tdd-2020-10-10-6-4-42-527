b89427af65af1fbae08cbb014544c90df3f39624
Cedric Don Coloma <colomce@oocl.com>
2020-10-15 23:15:53 +0800

    [MarsRover][Cedric Coloma] fix: Change test due to structural changes

--- src/test/java/com/afs/tdd/MarsRoverCommandTest.java
+++ src/test/java/com/afs/tdd/MarsRoverCommandTest.java
@@ -1,5 +1,9 @@
 package com.afs.tdd;
 
+import com.afs.tdd.commands.Jump;
+import com.afs.tdd.commands.Move;
+import com.afs.tdd.commands.TurnLeft;
+import com.afs.tdd.commands.TurnRight;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -9,11 +13,11 @@ class MarsRoverCommandTest {
     @Test
     void should_x_0_y_1_heading_N_when_runCommands_given_x_0_y_0_heading_N_commands_M() {
         //given
-        String commands = "M";
         MarsRover rover = new MarsRover(0, 0, "N");
+        ControlCenter controlCenter = new ControlCenter(new Move(rover));
 
         //when
-        rover.runCommands(commands);
+        rover.runCommands(controlCenter.send());
 
         //then
         assertEquals(0, rover.getLocationX());
@@ -24,11 +28,11 @@ class MarsRoverCommandTest {
     @Test
     void should_x_0_y_neg1_heading_S_when_runCommands_given_x_0_y_0_heading_S_commands_M() {
         //given
-        String commands = "M";
         MarsRover rover = new MarsRover(0, 0, "S");
+        ControlCenter controlCenter = new ControlCenter(new Move(rover));
 
         //when
-        rover.runCommands(commands);
+        rover.runCommands(controlCenter.send());
 
         //then
         assertEquals(0, rover.getLocationX());
@@ -39,11 +43,11 @@ class MarsRoverCommandTest {
     @Test
     void should_x_1_y_0_heading_E_when_runCommands_given_x_0_y_0_heading_E_commands_M() {
         //given
-        String commands = "M";
         MarsRover rover = new MarsRover(0, 0, "E");
+        ControlCenter controlCenter = new ControlCenter(new Move(rover));
 
         //when
-        rover.runCommands(commands);
+        rover.runCommands(controlCenter.send());
 
         //then
         assertEquals(1, rover.getLocationX());
@@ -54,11 +58,11 @@ class MarsRoverCommandTest {
     @Test
     void should_x_neg1_y_0_heading_W_when_runCommands_given_x_0_y_0_heading_W_commands_M() {
         //given
-        String commands = "M";
         MarsRover rover = new MarsRover(0, 0, "W");
+        ControlCenter controlCenter = new ControlCenter(new Move(rover));
 
         //when
-        rover.runCommands(commands);
+        rover.runCommands(controlCenter.send());
 
         //then
         assertEquals(-1, rover.getLocationX());
@@ -69,11 +73,11 @@ class MarsRoverCommandTest {
     @Test
     void should_x_0_y_0_heading_W_when_runCommands_given_x_0_y_0_heading_N_commands_L() {
         //given
-        String commands = "L";
         MarsRover rover = new MarsRover(0, 0, "N");
+        ControlCenter controlCenter = new ControlCenter(new TurnLeft(rover));
 
         //when
-        rover.runCommands(commands);
+        rover.runCommands(controlCenter.send());
 
         //then
         assertEquals(0, rover.getLocationX());
@@ -84,11 +88,11 @@ class MarsRoverCommandTest {
     @Test
     void should_x_0_y_0_heading_E_when_runCommands_given_x_0_y_0_heading_S_commands_L() {
         //given
-        String commands = "L";
         MarsRover rover = new MarsRover(0, 0, "S");
+        ControlCenter controlCenter = new ControlCenter(new TurnLeft(rover));
 
         //when
-        rover.runCommands(commands);
+        rover.runCommands(controlCenter.send());
 
         //then
         assertEquals(0, rover.getLocationX());
@@ -99,11 +103,11 @@ class MarsRoverCommandTest {
     @Test
     void should_x_0_y_0_heading_N_when_runCommands_given_x_0_y_0_heading_E_commands_L() {
         //given
-        String commands = "L";
         MarsRover rover = new MarsRover(0, 0, "E");
+        ControlCenter controlCenter = new ControlCenter(new TurnLeft(rover));
 
         //when
-        rover.runCommands(commands);
+        rover.runCommands(controlCenter.send());
 
         //then
         assertEquals(0, rover.getLocationX());
@@ -114,11 +118,11 @@ class MarsRoverCommandTest {
     @Test
     void should_x_0_y_0_heading_S_when_runCommands_given_x_0_y_0_heading_W_commands_L() {
         //given
-        String commands = "L";
         MarsRover rover = new MarsRover(0, 0, "W");
+        ControlCenter controlCenter = new ControlCenter(new TurnLeft(rover));
 
         //when
-        rover.runCommands(commands);
+        rover.runCommands(controlCenter.send());
 
         //then
         assertEquals(0, rover.getLocationX());
@@ -129,11 +133,11 @@ class MarsRoverCommandTest {
     @Test
     void should_x_0_y_0_heading_E_when_runCommands_given_x_0_y_0_heading_N_commands_R() {
         //given
-        String commands = "R";
         MarsRover rover = new MarsRover(0, 0, "N");
+        ControlCenter controlCenter = new ControlCenter(new TurnRight(rover));
 
         //when
-        rover.runCommands(commands);
+        rover.runCommands(controlCenter.send());
 
         //then
         assertEquals(0, rover.getLocationX());
@@ -144,11 +148,11 @@ class MarsRoverCommandTest {
     @Test
     void should_x_0_y_0_heading_W_when_runCommands_given_x_0_y_0_heading_S_commands_R() {
         //given
-        String commands = "R";
         MarsRover rover = new MarsRover(0, 0, "S");
+        ControlCenter controlCenter = new ControlCenter(new TurnRight(rover));
 
         //when
-        rover.runCommands(commands);
+        rover.runCommands(controlCenter.send());
 
         //then
         assertEquals(0, rover.getLocationX());
@@ -159,11 +163,11 @@ class MarsRoverCommandTest {
     @Test
     void should_x_0_y_0_heading_S_when_runCommands_given_x_0_y_0_heading_E_commands_R() {
         //given
-        String commands = "R";
         MarsRover rover = new MarsRover(0, 0, "E");
+        ControlCenter controlCenter = new ControlCenter(new TurnRight(rover));
 
         //when
-        rover.runCommands(commands);
+        rover.runCommands(controlCenter.send());
 
         //then
         assertEquals(0, rover.getLocationX());
@@ -174,11 +178,11 @@ class MarsRoverCommandTest {
     @Test
     void should_x_0_y_0_heading_N_when_runCommands_given_x_0_y_0_heading_W_commands_R() {
         //given
-        String commands = "R";
         MarsRover rover = new MarsRover(0, 0, "W");
+        ControlCenter controlCenter = new ControlCenter(new TurnRight(rover));
 
         //when
-        rover.runCommands(commands);
+        rover.runCommands(controlCenter.send());
 
         //then
         assertEquals(0, rover.getLocationX());
@@ -189,15 +193,13 @@ class MarsRoverCommandTest {
     @Test
     void should_throw_CommandNotDefinedException_when_runActions_given_x_0_y_0_heading_N_commands_F() {
         //given
-        String commands = "F";
         MarsRover rover = new MarsRover(0, 0, "N");
 
-        assertThrows(CommandNotDefinedException.class, () -> {
-            //when
-            rover.runCommands(commands);
-        });
+        //when
+        ControlCenter controlCenter = new ControlCenter(new Jump(rover));
 
         //then
+        assertThrows(CommandNotDefinedException.class, () -> rover.runCommands(controlCenter.send()));
         assertEquals(0, rover.getLocationX());
         assertEquals(0, rover.getLocationY());
         assertEquals("N", rover.getHeading());
@@ -206,11 +208,14 @@ class MarsRoverCommandTest {
     @Test
     void should_x_neg1_y_1_heading_N_when_runCommands_given_x_0_y_0_heading_N_commands_MLMR() {
         //given
-        String commands = "MLMR";
         MarsRover rover = new MarsRover(0, 0, "N");
-
+        ControlCenter controlCenter = new ControlCenter(
+                new Move(rover),
+                new TurnLeft(rover),
+                new Move(rover),
+                new TurnRight(rover));
         //when
-        rover.runCommands(commands);
+        rover.runCommands(controlCenter.send());
 
         //then
         assertEquals(-1, rover.getLocationX());
