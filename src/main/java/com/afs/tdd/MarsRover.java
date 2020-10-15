aa6f9aab569ba50ceb2b47c36dced9d5529f742d
Cedric Don Coloma <colomce@oocl.com>
2020-10-15 17:40:47 +0800

    [MarsRover][Cedric Coloma] test: x 0, y 1, heading N when x: 0, y: 0, heading: N, commands: M

---
+++ src/main/java/com/afs/tdd/MarsRover.java
@@ -0,0 +1,29 @@
+package com.afs.tdd;
+
+public class MarsRover {
+
+    private int x;
+    private int y;
+    private String heading;
+
+    public MarsRover(int x, int y, String heading) {
+        this.x = x;
+        this.y = y;
+        this.heading = heading;
+    }
+
+    public void runActions(String commands) {
+    }
+
+    public int getLocationX() {
+        return 0;
+    }
+
+    public int getLocationY() {
+        return 0;
+    }
+
+    public String getHeading() {
+        return null;
+    }
+}
--- src/test/java/com/afs/tdd/DemoTest.java
+++ src/test/java/com/afs/tdd/DemoTest.java
@@ -2,9 +2,21 @@ package com.afs.tdd;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DemoTest {
     @Test
-    void write_your_first_test() {
+    void should_x_0_y_1_heading_N_when_runActions_given_x_0_y_0_heading_N_commands_M() {
+        //given
+        String commands = "M";
+        MarsRover rover = new MarsRover(0,0,"N");
 
+        //when
+        rover.runActions(commands);
+
+        //then
+        assertEquals(0, rover.getLocationX());
+        assertEquals(1, rover.getLocationY());
+        assertEquals("N", rover.getHeading());
     }
 }
